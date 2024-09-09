330167fb460de3be3161d25cc2e08a6bdb45a03e
Aakriti <aakritiad01@gmail.com>
2024-09-09 14:36:57 +0545

    category implemented

---
+++ quotes.js
@@ -0,0 +1,42 @@
+const quotes = [
+  {
+    id: 1,
+    quote:
+      "Science is a way of thinking much more than it is a body of knowledge.",
+    author: "Carl Sagan",
+    category: "science",
+  },
+  {
+    id: 2,
+    quote: "The important thing is to never stop questioning.",
+    author: "Albert Einstein",
+    category: "science",
+  },
+  {
+    id: 3,
+    quote:
+      "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world.",
+    author: "Albert Einstein",
+    category: "inspiration",
+  },
+  {
+    id: 4,
+    quote: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+    category: "inspiration",
+  },
+  {
+    id: 5,
+    quote: "Somewhere, something incredible is waiting to be known.",
+    author: "Carl Sagan",
+    category: "science",
+  },
+  {
+    id: 6,
+    quote:
+      "Your time is limited, so don't waste it living someone else's life.",
+    author: "Steve Jobs",
+    category: "inspiration",
+  },
+];
+export default quotes;
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-document.getElementById('demo').textContent = 'JavaScript is working!';
+import quotes from "./quotes.js";
+
+document.getElementById("fetch-quote").addEventListener("click", function () {
+  const selectedCategory = document.querySelector(
+    'input[name="radio"]:checked'
+  );
+
+  if (selectedCategory) {
+    const category =
+      selectedCategory.id === "fetch-science-quote" ? "science" : "inspiration";
+    const filteredQuotes = quotes.filter(
+      (quote) => quote.category === category
+    );
+    const randomIndex = Math.floor(Math.random() * filteredQuotes.length);
+    const randomQuote = filteredQuotes[randomIndex];
+    document.getElementById(
+      "demo"
+    ).textContent = `"${randomQuote.quote}" - ${randomQuote.author}`;
+  } else {
+    document.getElementById("demo").textContent =
+      "Please select a category first.";
+  }
+});
